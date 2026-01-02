2eb56b6543a371c810c6f4b8d6f8a6e8a50a1ced
Boaz Briddick <boazb2@outlook.com>
2026-01-02 17:50:16 -0600

    Initial Commit

---
+++ Reforged/ReforgedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReforgedApp.swift
+//  Reforged
+//
+//  Created by Boaz Briddick on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ReforgedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
